fe53ec3d719fa0e44a1c5137640564d9314c1572
Chandhok <davinder.chandhok@zuehlke.com>
2019-02-28 23:53:58 +0100

    Add ADSR code + test timer

--- spres_synth_contest.ino
+++ spres_synth_contest.ino
@@ -22,13 +22,88 @@
 
 AudioClass *theAudio;
 
-static const uint8_t soft_1 = PIN_A4;
-static const uint8_t fsr_1 = PIN_A5;
+static const uint8_t soft_2 = PIN_A4;
+static const uint8_t soft_1 = PIN_A5;
 
 int note_1 = 0;
+int note_2 = 0;
 int note_1_on = 0;
+int note_2_on = 0;
 int vibrato_1 = 0;
 float set_note = 94;
+float curr_time = 0;
+unsigned int curr_clock = 0;
+
+struct envelopeADSR{
+  float attackTime;
+  float decayTime;
+  float sustainAmp;
+  float releaseTime;
+  float attackAmp;
+  
+  float triggerOnTime;
+  float triggerOffTime;
+  bool noteStatus;
+
+  envelopeADSR(){
+    attackTime = 0.100;
+    decayTime = 0.01;
+    attackAmp = 1.0;
+    sustainAmp = 0.8;
+    releaseTime = 0.200;
+    triggerOnTime = 0.0;
+    triggerOffTime = 0.0;
+    noteStatus = false;
+  }
+
+  float getAmp(float dTime){
+    float amp = 0.0;
+    float lifeTime = dTime - triggerOnTime;
+
+    if(noteStatus){
+      // ADS
+
+      // Attack
+      if(lifeTime <= attackTime){
+        amp = (lifeTime/attackTime)*attackAmp;
+      }
+
+      // Decay
+      if(lifeTime > attackTime && lifeTime <= (attackTime + decayTime)){
+        amp = ((lifeTime - attackTime)/decayTime)*(sustainAmp - attackAmp) + attackAmp;
+      }
+
+      // Sustain
+      if(lifeTime > (attackTime + decayTime)){
+        amp = sustainAmp;
+      }
+    }
+
+    else{
+      // Release
+      amp = ((dTime - triggerOffTime)/releaseTime)*(0.0 - sustainAmp) + sustainAmp;
+    }
+
+    if(amp <= 0.0001){
+      amp = 0;
+    }
+    
+    return amp;
+  }
+  
+  void noteOn(float dTimeOn){
+    triggerOnTime = dTimeOn;
+    noteStatus = true;
+  }
+
+  void noteOff(float dTimeOff){
+    triggerOffTime = dTimeOff;
+    noteStatus = false;
+  }
+};
+
+unsigned int cycle = 25;
+int timerInt = 0;
 
 class Score
 {
@@ -53,29 +128,6 @@ public:
 private:
 
   int pos;
-/*
-  Note data[17] =
-  {
-    {262,500},
-    {294,500},
-    {330,500},
-    {349,500},
-    {392,500},
-    {440,500},
-    {494,500},
-    {523,1000},
-
-    {523,500},
-    {494,500},
-    {440,500},
-    {392,500},
-    {349,500},
-    {330,500},
-    {294,500},
-    {262,1000},
-
-    {0,0}
-  };*/
  
   Note data[5] =
   {
@@ -89,10 +141,12 @@ private:
 };
 
 Score theScore;
+envelopeADSR envelope;
 
 void setup()
 {
-  
+
+  attachTimerInterrupt(isr,cycle);
   theAudio = AudioClass::getInstance();
   theAudio->begin();
   puts("initialization Audio Library");
@@ -103,12 +157,19 @@ void setup()
    
 }
 
+unsigned int isr(void) {
+  timerInt = 1;
+  curr_clock += 25;
+  return cycle;
+}
+
 void loop()
 {
   puts("loop!!");
+  printf("Current clock = %d", curr_clock);
 
   note_1 = analogRead(soft_1);
-  vibrato_1 = analogRead(fsr_1);
+  note_2 = analogRead(soft_2);
   printf("softpot = %d, fsr = %d\n", note_1, vibrato_1);
   //Score::Note theNote = theScore.get();
   //if (theNote.fs == 0) {
@@ -117,13 +178,18 @@ void loop()
   //}
   if(note_1 > 1000 or note_1 < 20){
     note_1_on = 0;
+    //duration = 0;
   }
   else{
     note_1_on = 1;
-    set_note = 94 + (3991*note_1)/ADC_PREC;
+    set_note = 150 + (450*note_1)/ADC_PREC;
+    //duration += 1;
   }
-  
-  theAudio->setBeep(note_1_on,-40,set_note);
+
+  curr_time = 0.01;
+  envelope.noteOn(curr_time);
+  float outputAmp = -40*envelope.getAmp(curr_time);
+  theAudio->setBeep(note_1_on,outputAmp,set_note);
   //usleep(theNote.time * 1000);
   //theAudio->setBeep(0,0,0);
   //usleep(1000);

6b40f962d9ce73bab4e331297f95b3fd96db203f
Chandhok <davinder.chandhok@zuehlke.com>
2019-03-04 02:33:26 +0100

    Adjust ADSR + add FSR measurements

--- spres_synth_contest.ino
+++ spres_synth_contest.ino
@@ -24,32 +24,88 @@ AudioClass *theAudio;
 
 static const uint8_t soft_2 = PIN_A4;
 static const uint8_t soft_1 = PIN_A5;
+static const uint8_t fsr_1 = PIN_A3;
 
 int note_1 = 0;
 int note_2 = 0;
+int fsr_val = 0;
 int note_1_on = 0;
 int note_2_on = 0;
 int vibrato_1 = 0;
-float set_note = 94;
+float vibratoAmp = 1;
+float envelopeAmp = 1;
+float set_note = 94; // Minimum value accepted by setBeep
 float curr_time = 0;
 unsigned int curr_clock = 0;
+float outputAmp = 0.0;
+
+float note_freqs[12]{
+  261.62,
+  277.18,
+  293.66,
+  311.13,
+  
+  329.63,
+  349.23,
+  369.99,
+  391.99,
+  
+  415.30,
+  440.00,
+  466.16,
+  493.88
+};
+
+struct vibrato{
+  float vibStartTime;
+  float freq;
+  bool noteStatus;
+
+  vibrato(){
+    vibStartTime = 0;
+    freq = 0;
+    noteStatus = false;
+  }
+  float vibAmp(float dTimeOn){
+    if(noteStatus){
+      return sin(62.8*dTimeOn);
+    }
+    else{
+      return 1;
+    }
+  }
+  
+  void noteOn(float dTimeOn){
+    vibStartTime = dTimeOn;
+    noteStatus = true;
+  }
+
+  void noteOff(){
+    freq = 0;
+    noteStatus = false;
+  }
+  
+  bool getStatus(void){
+    return noteStatus;
+  }
+};
 
 struct envelopeADSR{
   float attackTime;
   float decayTime;
+  float attackAmp;
   float sustainAmp;
   float releaseTime;
-  float attackAmp;
   
   float triggerOnTime;
   float triggerOffTime;
   bool noteStatus;
 
   envelopeADSR(){
-    attackTime = 0.050;
-    decayTime = 0.05;
-    attackAmp = 1.0;
-    sustainAmp = 0.95;
+    attackTime = 0.250;
+    decayTime = 0.5;
+    attackAmp = -25;
+    sustainAmp = -37;
     releaseTime = 0.500;
     triggerOnTime = 0.0;
     triggerOffTime = 0.0;
@@ -84,7 +140,7 @@ struct envelopeADSR{
       amp = ((dTime - triggerOffTime)/releaseTime)*(0.0 - sustainAmp) + sustainAmp;
     }
 
-    if(amp <= 0.0001){
+    if(amp <= 0.0001 && amp >= -0.0001){
       amp = 0;
     }
     
@@ -108,43 +164,8 @@ struct envelopeADSR{
 unsigned int cycle = 25;
 int timerInt = 0;
 
-class Score
-{
-public:
-
-  typedef struct {
-    int fs;  
-    int time;
-  } Note;
-
-  void init(){
-    pos = 0;
-  }
-  
-  Note get(){
-    if(pos == 20){
-      pos = 0;
-    }
-    return data[pos++];
-  }
-  
-private:
-
-  int pos;
- 
-  Note data[5] =
-  {
-    {100,1000},
-    {120,1000},
-    {150,1000},
-    {180,1000},
-    {2500,1000}
-  };
-  
-};
-
-Score theScore;
 envelopeADSR envelope;
+vibrato vibratoInst;
 
 void setup()
 {
@@ -156,7 +177,7 @@ void setup()
 
   theAudio->setPlayerMode(AS_SETPLAYER_OUTPUTDEVICE_SPHP);
 
-  theScore.init();
+  //theScore.init();
    
 }
 
@@ -166,42 +187,76 @@ unsigned int isr(void) {
   return cycle;
 }
 
+float get_freq(int analog_val){
+  if(analog_val > 980){
+    return note_freqs[11];
+  }
+  if(analog_val > 960){
+    return note_freqs[10];
+  }
+  if(analog_val > 940){
+    return note_freqs[9];
+  }
+  if(analog_val > 910){
+    return note_freqs[8];
+  }
+  if(analog_val > 888){
+    return note_freqs[7];
+  }
+  if(analog_val > 850){
+    return note_freqs[6];
+  }
+  if(analog_val > 800){
+    return note_freqs[5];
+  }
+  if(analog_val > 733){
+    return note_freqs[4];
+  }
+  if(analog_val > 630){
+    return note_freqs[3];
+  }
+  if(analog_val > 475){
+    return note_freqs[2];
+  }
+  if(analog_val > 240){
+    return note_freqs[1];
+  }
+  else{
+    return note_freqs[0];
+  }
+
+}
+
 void loop()
 {
   puts("loop!!");
-  printf("\t\t\t\t\tCurrent clock = %d\n", curr_clock);
+  printf("\t\t\t\tCurrent clock = %d, amp %d\n", curr_clock, 100*vibratoAmp);
 
   note_1 = analogRead(soft_1);
   note_2 = analogRead(soft_2);
-  printf("softpot_1 = %d, softpot_2 = %d\n", note_1, note_2);
-  //Score::Note theNote = theScore.get();
-  //if (theNote.fs == 0) {
-    //puts("End,");
-    //exit(1);
-  //}
+  fsr_val = analogRead(fsr_1);
+  printf("envelopeAmp = %d\n", 100*envelopeAmp);
   curr_time = 0.000001*curr_clock;
-  if(note_2 > 1000 or note_2 < 20){
+  if(note_2 > 1000 or note_2 < 1){
     note_2_on = 0;
     if(envelope.getStatus()){
       envelope.noteOff(curr_time);
+      vibratoInst.noteOff();
     }
-    //duration = 0;
   }
   else{
     note_2_on = 1;
     if(!envelope.getStatus()){
       envelope.noteOn(curr_time);
+      vibratoInst.noteOn(curr_time);
     }
-    set_note = 150 + (450*note_2)/ADC_PREC;
-    //duration += 1;
+    set_note = get_freq(note_2);
   }
 
   
-  
-  float outputAmp = -40*envelope.getAmp(curr_time);
-  theAudio->setBeep(note_2_on,outputAmp,set_note);
-  //usleep(theNote.time * 1000);
-  //theAudio->setBeep(0,0,0);
-  //usleep(1000);
+  vibratoAmp = vibratoInst.vibAmp(curr_time);
+  envelopeAmp = envelope.getAmp(curr_time);
+  outputAmp = vibratoAmp*envelopeAmp;
 
+  theAudio->setBeep(note_2_on,outputAmp,set_note);
 }
